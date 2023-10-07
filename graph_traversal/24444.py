d38b84b111cb0a13b1af5c131c6d1ef41ca5dd6c
Minyoung Baek <kevinbaek01@gmail.com>
2023-10-07 23:38:54 +0900

    add q24444

---
+++ graph_traversal/24444.py
@@ -0,0 +1,29 @@
+import sys
+from collections import deque
+
+def bfs(graph, start):
+    queue = deque()
+    cnt = 1
+    queue.append(start)
+    while queue:
+        node = queue[0]
+        queue.popleft()
+        if visited[node] == 0:
+            visited[node] = cnt
+            cnt += 1
+            queue.extend(graph[node])
+
+N, M, R = map(int, sys.stdin.readline().split())
+graph = [[] for i in range(N+1)]
+for _ in range(M):
+    u, v = map(int, sys.stdin.readline().split())
+    graph[u].append(v)
+    graph[v].append(u)
+for arr in graph:
+    arr.sort()
+
+visited = [0]*(N+1)
+bfs(graph, R)
+
+for i in range(1, N+1):
+    print(visited[i])
