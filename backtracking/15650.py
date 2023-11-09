b520fc1b45a39214971b3192f6ffb1e2a57988a9
kevinb <kevinbaek01@gmail.com>
2023-11-09 17:11:53 +0900

    change to backtracking, add q15650

---
+++ backtracking/15650.py
@@ -0,0 +1,18 @@
+def dfs(start):
+    if len(s) == M:
+        print(' '.join(map(str, s)))
+        return
+    for i in range(start, N+1):
+        if visited[i]:
+            continue
+        visited[i] = True
+        s.append(i)
+        dfs(i+1)
+        s.pop()
+        visited[i] = False
+
+N, M = map(int, input().split())
+s = []
+visited = [False] * (N+1)
+
+dfs(1)
